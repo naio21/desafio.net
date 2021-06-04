ed0011ec25c7bbf625151967265d8eab76fbf7bd
naio21 <ivanbprado@gmail.com>
2021-06-04 16:31:49 -0300

    Testes automatizados finalizados. Aplicação finalizada.

--- CNABParser/CNABParser.Tests/HomeControllerTest.cs
+++ CNABParser/CNABParser.Tests/HomeControllerTest.cs
@@ -1,12 +1,10 @@
 using CNABParser.Business.Services;
-using CNABParser.Data.Models;
 using CNABParser.Data.Repositories;
 using CNABParser.MVC.Controllers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Moq;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,13 +17,13 @@ namespace CNABParser.Tests
         private readonly TransacaoRepository _repository;
         private readonly TransacaoService _service;
         private readonly IWebHostEnvironment _environment;
+        private readonly CNABContext _dbContext;
         private readonly string _appPath;
 
         public HomeControllerTest()
         {
             var optionsDbContext = new DbContextOptionsBuilder<CNABContext>().UseInMemoryDatabase(databaseName: this.GetType().FullName).Options;
-            CNABContext _dbContext = new CNABContext(optionsDbContext);
-            //CNABContext _dbContext = new CNABContext(new DbContextOptions<CNABContext>());
+            _dbContext = new CNABContext(optionsDbContext);
             _repository = new TransacaoRepository(_dbContext);
             _service = new TransacaoService(_repository);
             Mock<IWebHostEnvironment> webHostMock = new Mock<IWebHostEnvironment>();
@@ -34,9 +32,8 @@ namespace CNABParser.Tests
             _environment = webHostMock.Object;
         }
 
-        private Mock<IFormFile> mockFormFile(string formFilePath)
+        private IFormFile formFile(string formFilePath)
         {
-            Mock<IFormFile> mockFormFile = new Mock<IFormFile>();
             FileInfo fi = new FileInfo(formFilePath);
             string fileName = fi.Name;
             string content = File.ReadAllText(formFilePath);
@@ -45,25 +42,36 @@ namespace CNABParser.Tests
             sw.Write((content));
             sw.Flush();
             ms.Position = 0;
-            mockFormFile.Setup(_ => _.OpenReadStream()).Returns(ms);
-            mockFormFile.Setup(_ => _.FileName).Returns(fileName);
-            mockFormFile.Setup(_ => _.Length).Returns(fi.Length);
-            mockFormFile.Setup(_ => _.ContentDisposition).Returns($"form-data; name=\"arquivo\"; filename=\"{fileName}\"");
-            mockFormFile.Setup(_ => _.ContentType).Returns($"text/plain");
-            return mockFormFile;
+            FormFile ret = new FormFile(ms, 0, fi.Length, fileName, fileName)
+            {
+                Headers = new HeaderDictionary(),
+                ContentType = "text/plain"
+            };
+            return ret;
         }
 
         [Fact(DisplayName = "Upload e Parse realizados com sucesso (arquivo padrão CNAB)")]
         public async Task UploadAndParseSuccessful()
         {
             //Arrange
-            Mock<IFormFile> formFile = mockFormFile($"{_appPath}\\upload\\CNABSuccess.txt");
+            IFormFile iFormFile = formFile($"{_appPath}\\upload\\CNABSuccess.txt");
             //Act
             HomeController homeController = new HomeController(_service, _environment);
-            await homeController.ArquivoForm(formFile.Object);
+            await homeController.ArquivoForm(iFormFile);
             //Assert
-            IEnumerable<Transacao> transacoes = await _repository.GetAllTransacoesAsync();
-            Assert.True(transacoes.Any());
+            Assert.True(_dbContext.Transacoes.Any());
+        }
+
+        [Fact(DisplayName = "Upload e Parse falharam (arquivo fora do padrão CNAB)")]
+        public async Task UploadAndParseFailed()
+        {
+            //Arrange
+            IFormFile iFormFile = formFile($"{_appPath}\\upload\\CNABFail.txt");
+            //Act
+            HomeController homeController = new HomeController(_service, _environment);
+            await homeController.ArquivoForm(iFormFile);
+            //Assert
+            Assert.False(_dbContext.Transacoes.Any());
         }
     }
 }
